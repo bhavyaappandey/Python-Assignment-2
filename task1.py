76943ecce5f938e24af5166723a603a0256838bf
bhavyaappandey <bhavyaappandey@gmail.com>
2025-12-23 18:42:39 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -3,6 +3,6 @@
 num = int(input("Enter a number: "))
 
 if num % 2 == 0:
-    print("The number is even")
+    print(num, "is an even number.")
 else:
-    print("The number is odd")
+    print(num, "is an odd number.")
