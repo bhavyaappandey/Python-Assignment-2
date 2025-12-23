fef11c22f8873aa99dcad8ee84c3ea4b8efe802a
bhavyaappandey <bhavyaappandey@gmail.com>
2025-12-23 17:38:57 +0530

    task1.py

--- task1.py
+++ task1.py
@@ -1,10 +1,8 @@
-# Task 1: Check number type using conditions
+# Task 1: Check if a number is even or odd using conditions
 
 num = int(input("Enter a number: "))
 
-if num > 0:
-    print("The number is positive")
-elif num < 0:
-    print("The number is negative")
+if num % 2 == 0:
+    print("The number is even")
 else:
-    print("The number is zero")
+    print("The number is odd")
